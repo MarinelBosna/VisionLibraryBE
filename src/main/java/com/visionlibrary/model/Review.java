fccddbb65d6d4dce088054f0897613ae2c3ad6a8
MarinelBosna <marinel.bosna00@e-uvt.ro>
2022-06-02 14:09:32 +0300

    test commit

--- src/main/java/com/visionlibrary/model/Review.java
+++ src/main/java/com/visionlibrary/model/Review.java
@@ -1,4 +1,13 @@
 package com.visionlibrary.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table
+@Entity
 public class Review {
+
+    @Id
+    private Long idReview;
 }
