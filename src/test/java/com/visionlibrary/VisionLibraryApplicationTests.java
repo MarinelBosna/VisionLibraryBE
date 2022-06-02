d569aa8837bd27d6550b8a3321543635fff8e1f3
MarinelBosna <marinel.bosna00@e-uvt.ro>
2022-06-02 14:15:54 +0300

    Main class and main test class

---
+++ src/test/java/com/visionlibrary/VisionLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.visionlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisionLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
